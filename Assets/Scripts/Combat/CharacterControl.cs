4dc53fb906a08f93fddbae4d13ef53cea70abc2f
Brandon-JB <bbourdage@ltu.edu>
2024-01-16 16:17:02 -0500

    Added basics of fighting
    Added the ability for the player to hit the enemy
    Added the ability for the enemy to lose health and have vulnerability to certain damage

--- Assets/Scripts/Combat/CharacterControl.cs
+++ Assets/Scripts/Combat/CharacterControl.cs
@@ -7,14 +7,42 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     //static is necessary to put variables in a constructor
-    static public int[] valueList = { 69, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    //static public string Name;
+    static public int[] valueList = {
+        /*Strength*/ 2,
+        /*Magic Attack*/ 1,
+        /*Defense*/ 2, 
+        /*Speed*/ 3, 
+        /*Health*/ 4, 
+        /*MaxHealth*/ 5,
+        /*Mana*/ 6,
+        /*MaxMana*/ 7,
+        /*EXP*/ 0,
+        /*LvlUpThreshold*/ 10 };
     public Character charData = new Character("Bob", valueList, "Cube", "Is a cube");
     //public TextMeshPro text;
     public TMP_Text text;
 
+    public EnemyControl targetEnemy;
+
     private void Start()
     {
         text.text = "Health: " + charData.stats["Health"];
+    }
+
+    //A physical attack that uses the strength stat
+    public void PhysicalAttack()
+    {
+        targetEnemy.PhysicalHurt(charData.stats["Strength"]);
+    }
+
+    //A magical attack that uses the magic attack stat and is the element of fire
+    public void MagicAttack()
+    {
+        targetEnemy.MagicHurt(charData.stats["Magic Attack"], charData.magicType);
+    }
+
+    public void Hit()
+    {
+
     }
 }
