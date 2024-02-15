234c69f11376bcd25f77ad3729221d9d7258d4e6
AlexanderOr <aortwine@ltu.edu>
2024-02-15 16:38:43 -0500

    Finishing up 2nd level

--- Assets/Scripts/Overworld/SwitchScript.cs
+++ Assets/Scripts/Overworld/SwitchScript.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class SwitchScript : MonoBehaviour
 {
 
     public GameObject Door;
+    //public RigidbodyConstraints2D pos;
+    
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Switch")
         {
             Debug.Log("Collided");
+            //pos = RigidbodyConstraints2D.FreezePosition;
             Destroy(Door);
         }
     }
