2d4b621065ec0eec6127f9fb1ed2f70b43f01106
AlexanderOr <aortwine@ltu.edu>
2024-02-20 16:36:22 -0500

    edits to boss positioning

--- Assets/Scripts/Overworld/PlayerController.cs
+++ Assets/Scripts/Overworld/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 5f;
     public Rigidbody2D rb;
-    private bool isMoving;
     private bool isfrozen;
 
     private Vector2 input;
@@ -46,32 +45,6 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + input * movementSpeed * Time.fixedDeltaTime);
-    }
-
-    IEnumerator Move(Vector3 targetPos)
-    {
-        isMoving = true;
-
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, movementSpeed * Time.deltaTime);
-            yield return null;
-        }
-
-        transform.position = targetPos;
-
-        isMoving = false;
-    }
-
-    private bool isWalkable(Vector3 targetPos)
-    {
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
-        {
-            return false;
-        }
-
-        
-        return true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
