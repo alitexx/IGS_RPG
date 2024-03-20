7ae59a075e09200975aa1dcc4f4ca6e398ee2d6b
AlexanderOr <aortwine@ltu.edu>
2024-03-20 15:52:58 -0400

    Set up Tutorial Level
    Also changed the level script to have an area around it where the play can press space to interact. no more hugging the lever to flip it.

--- Assets/Scripts/Overworld/LeverScript.cs
+++ Assets/Scripts/Overworld/LeverScript.cs
@@ -5,7 +5,23 @@ using UnityEngine;
 public class LeverScript : MonoBehaviour
 {
     public GameObject Door;
+    public GameObject Target;
+    float maxDistance = 1.5f;
+    float DistanceBetweenObjects;
+
+    private void Update()
+    {
+        
+        DistanceBetweenObjects = Vector3.Distance(transform.position, Target.transform.position);
+
+        if (DistanceBetweenObjects <= maxDistance && Input.GetKeyDown("space"))
+        {
+            Debug.Log(":D");
+            Destroy(Door);
+        }
     
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && Input.GetKeyDown("space"))
