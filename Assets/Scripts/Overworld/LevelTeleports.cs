760ed39681b39b39027577ba76618acc03237c59
AlexanderOr <aortwine@ltu.edu>
2024-03-21 16:05:16 -0400

    Fixed stuff

--- Assets/Scripts/Overworld/LevelTeleports.cs
+++ Assets/Scripts/Overworld/LevelTeleports.cs
@@ -9,7 +9,11 @@ public class LevelTeleports : MonoBehaviour
     public Transform Teleport1;
     public Transform Teleport2;
     public GameObject portalParent;
+    public Collider2D Player;
     private Transform destination;
+
+    public GameObject ContinueUI;
+    public PlayerController PlayerController;
 
     public bool is2;
     public float distance = 0.2f;
@@ -27,16 +31,30 @@ public class LevelTeleports : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
+    public void OnTriggerEnter2D(Collider2D col)
     {
+
         if (Vector2.Distance(transform.position, col.transform.position) > distance)
         {
-            col.transform.position = new Vector3(destination.position.x, destination.position.y);
-            Destroy(portalParent);
+            ContinueUI.SetActive(true);
+            PlayerController.isfrozen = true;
         }
         
     }
 
+    public void Next()
+    {
+        PlayerController.isfrozen = false;
+        ContinueUI.SetActive(false);
+        Player.transform.position = new Vector3(destination.position.x, destination.position.y);
+        Destroy(portalParent);
 
+    }
 
+    public void Stay()
+    {
+        PlayerController.isfrozen = false;
+        ContinueUI.SetActive(false);
+        return;
+    }
 }
