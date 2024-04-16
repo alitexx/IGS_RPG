3c161f48ce2a53acbc023cdd4658c07b532ab7fb
AlexanderOr <aortwine@ltu.edu>
2024-04-16 15:48:38 -0400

    updated interaction
    Lever and saves are now on the interact key, and gargoyles snap to switches.

--- Assets/Scripts/Overworld/SavepointScript.cs
+++ Assets/Scripts/Overworld/SavepointScript.cs
@@ -19,7 +19,7 @@ public class SavepointScript : MonoBehaviour
 
         DistanceBetweenObjects = Vector3.Distance(transform.position, Target.transform.position);
 
-        if (DistanceBetweenObjects <= maxDistance && Input.GetKeyDown("space"))
+        if (DistanceBetweenObjects <= maxDistance && Input.GetKeyDown(audioStatics.interractButton))
         {
             //call function to heal, and eventually save.
             levelManager.FullHeal();
