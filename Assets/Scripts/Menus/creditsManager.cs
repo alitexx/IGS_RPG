00ff4a364426268f2ff27a4f57c574fcd20c4499
Katie T <kttackett123@gmail.com>
2024-04-19 14:29:28 -0400

    editing credits for final text

--- Assets/Scripts/Menus/creditsManager.cs
+++ Assets/Scripts/Menus/creditsManager.cs
@@ -62,6 +62,7 @@ private void OnEnable()
                 charactersOnTitle[0].SetActive(false);
                 charactersOnTitle[1].SetActive(false);
                 charactersOnTitle[2].SetActive(false);
+                endingText.text = "";
                 break;
             case 1:
                 charactersOnTitle[0].SetActive(false);
@@ -81,6 +82,9 @@ private void OnEnable()
                 break;
             case 5:
                 charactersOnTitle[2].SetActive(false);
+                break;
+            case 6:
+                endingText.text = "After the events at the lich’s tower, Alan takes a much-needed break from his duties to prioritize his mental well-being. This time is short-lived, as Alan tends to find himself in the middle of conflict, fighting for the side of justice. He would go on many adventures, but unlike this quest, the temptation to revive his mentor had vanished. He would sometimes get wistful reminiscing about Leora, but he accepted the past was the past and it was time to move on.\r\n\r\nOnce their celebration concluded, Kisa ran home to tell her family of her journey. They were dismissive of her accomplishments and critical of her desire to continue the dangerous life of an adventurer. Kisa was furious, especially since she went through this ordeal to impress them. She ran away days later, choosing a life of excitement over a life of normalcy.\r\n\r\nNicol disappeared soon after the party’s celebration, continuing his carefree exploration of this world. He still treasures the bond he has with his companions, somehow winding up back in Isen for holidays to reunite with them. Despite all of his journey and experiences, Nicol never lost his childlike wonder for uncovering the secrets of the world\r\n\r\nSophie held a small funeral for her fallen students on her arrival home, still feeling a twinge of guilt; if only she knew of the danger awaiting them, she could have prevented their deaths. Twas a fleeting thought, as she knew she could not change the past. Sophie reopened her dojo and expected this to become a distant memory, yet she found herself reminiscing on the fond times with her companions. It was then that she knew how to respond to Kisa, and finally told Kisa the words she wanted to hear.\r\n";
                 break;
         }
         audioManager.playBGM("T10");
