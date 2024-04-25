e97758dfcd33cf9e35b7bb99fbd7a25979ed5548
Katie T <kttackett123@gmail.com>
2024-04-25 15:37:32 -0400

    fixed error with delete save

--- Assets/Scripts/Menus/fadeInScript.cs
+++ Assets/Scripts/Menus/fadeInScript.cs
@@ -6,24 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class fadeInScript : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup fadeIn;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<CanvasGroup>().alpha = 1;
-        this.GetComponent<CanvasGroup>().DOFade(0, 1f);
+        if (fadeIn)
+        {
+            fadeIn.alpha = 1;
+            fadeIn.DOFade(0, 1f);
+        }
     }
 
     public void fadeToRPGWorld()
     {
-        this.GetComponent<CanvasGroup>().DOFade(1, 1f).OnComplete(() =>
+        fadeIn.blocksRaycasts = true;
+        fadeIn.DOFade(1, 1f).OnComplete(() =>
             {
+                fadeIn.DOKill();
                 SceneManager.LoadScene("RPG_World");
             });
     }
     public void fadeToMainMenu()
     {
-        this.GetComponent<CanvasGroup>().DOFade(1, 1f).OnComplete(() =>
+        fadeIn.blocksRaycasts = true;
+        fadeIn.DOFade(1, 1f).OnComplete(() =>
         {
+            fadeIn.DOKill();
+            PauseMenu.GamePaused = false;
             SceneManager.LoadScene("TitleScreen");
         });
     }
