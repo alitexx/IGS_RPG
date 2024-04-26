a2bdbe31ae6a42baf3e93debb5d5d0878e741129
Katie T <kttackett123@gmail.com>
2024-04-25 23:44:53 -0400

    added comments to potentially make enemies run from player
    for the player level, we could do the enemies run away if the players level is greater than 3 * floor level. so enemies on floor 1 would run when the player reaches level 4, level 7 on floor 2, level 10 on floor 3, and 13 on floor 4. I finished the game at level 11 doing the genocide run, so i think this is pretty good honestly

--- Assets/Scripts/Overworld/EnemyMovement.cs
+++ Assets/Scripts/Overworld/EnemyMovement.cs
@@ -78,3 +78,67 @@ public class EnemyMovement : MonoBehaviour
         
     }
 }
+
+//what mr gpt told me to do
+//i think lines 125-128 is where the major changes are
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class EnemyMovement : MonoBehaviour
+//{
+//    public float Speed = 3f;
+//    public Rigidbody2D TargetRB;
+//    public Rigidbody2D EnemyRB;
+//    public GameObject Target;
+//    public float maxDistance = 5f;
+//    public float fleeDistanceThreshold = 10f; // Adjust this value as needed
+//    public PlayerController PlayerController;
+//    private Animator animator;
+//    private Vector2 input;
+
+//    private void Start()
+//    {
+//        animator = GetComponent<Animator>();
+//        animator.SetFloat("moveX", 0);
+//        animator.SetFloat("moveY", 0);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        float distanceToTarget = Vector3.Distance(transform.position, Target.transform.position);
+
+//        if (distanceToTarget > maxDistance)
+//        {
+//            animator.SetBool("isMoving", false);
+//            return; // No need to continue if the player is out of range
+//        }
+
+//        if (!PlayerController.isfrozen)
+//        {
+//            if (distanceToTarget <= maxDistance)
+//            {
+//                Vector3 moveDirection = (transform.position - TargetRB.transform.position).normalized;
+
+//                if (distanceToTarget > fleeDistanceThreshold && PlayerController.level >= 10)
+//                {
+//                    // Player is high level, so flee from the player
+//                    EnemyRB.transform.position = Vector2.MoveTowards(EnemyRB.transform.position, transform.position + moveDirection * fleeDistanceThreshold, Speed * Time.deltaTime);
+//                }
+//                else
+//                {
+//                    // Player is not high level or too close, so approach the player
+//                    EnemyRB.transform.position = Vector2.MoveTowards(EnemyRB.transform.position, TargetRB.transform.position, Speed * Time.deltaTime);
+//                }
+
+//                // Animations
+//                animator.SetBool("isMoving", true);
+//                animator.SetFloat("moveX", -moveDirection.x);
+//                animator.SetFloat("moveY", -moveDirection.y);
+//                animator.SetFloat("speed", Speed);
+//            }
+//        }
+//    }
+//}
